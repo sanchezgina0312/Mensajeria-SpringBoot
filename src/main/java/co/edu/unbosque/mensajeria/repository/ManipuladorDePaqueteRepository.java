af456dd8e5d71ecb35ac62dc9ceff2596426e509
LisethBuitrago <lisethramireeez@gmail.com>
2026-03-21 09:34:31 -0500

    conductor y paquete alimenticio repository

--- src/main/java/co/edu/unbosque/mensajeria/repository/ManipuladorDePaqueteRepository.java
+++ src/main/java/co/edu/unbosque/mensajeria/repository/ManipuladorDePaqueteRepository.java
@@ -1,9 +1,20 @@
 package co.edu.unbosque.mensajeria.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import co.edu.unbosque.mensajeria.entity.ManipuladorDePaquete;
 
 public interface ManipuladorDePaqueteRepository extends CrudRepository<ManipuladorDePaquete, Long> {
 
+	public Optional<List<ManipuladorDePaquete>> findByNombre(String nombre);
+	public Optional<List<ManipuladorDePaquete>> findByCedula(String cedula);
+	public Optional<List<ManipuladorDePaquete>> findByCorreo(String correo);	
+	public Optional<List<ManipuladorDePaquete>> findByTelefono(String telefono);		
+	public Optional<List<ManipuladorDePaquete>> findByTurno(char turno);	
+	public Optional<List<ManipuladorDePaquete>> findByTipoManipulador(String tipoManipulador);
+	
+	public Optional<List<ManipuladorDePaquete>> findByNombreYCedula(String nombre, String cedula);	
 }
--- src/main/java/co/edu/unbosque/mensajeria/repository/PaqueteAlimenticioRepository.java
+++ src/main/java/co/edu/unbosque/mensajeria/repository/PaqueteAlimenticioRepository.java
@@ -1,7 +1,19 @@
 package co.edu.unbosque.mensajeria.repository;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
+
 import co.edu.unbosque.mensajeria.entity.PaqueteAlimenticio;
 
 public interface PaqueteAlimenticioRepository extends CrudRepository<PaqueteAlimenticio, Long> {
-
+	
+	public Optional<List<PaqueteAlimenticio>>findByPrecioEnvio(int precioEnvio);	
+	public Optional<List<PaqueteAlimenticio>>findByDireccionDestino(String direccionDestino);
+	public Optional<List<PaqueteAlimenticio>>findByTamanio(String tamanio);
+	public Optional<List<PaqueteAlimenticio>>findByFechaCreacionPedido(LocalDateTime fechaCreacionPedido);
+	public Optional<List<PaqueteAlimenticio>>findByFechaEstimadaEntrega(LocalDateTime fechaEstimadaEntrega);
+	public Optional<List<PaqueteAlimenticio>>findBySeEnviaHoy(boolean seEnviaHoy);	
+	public Optional<List<PaqueteAlimenticio>>findByTipoDeAlimento(String tipoDeAlimento);
 }
