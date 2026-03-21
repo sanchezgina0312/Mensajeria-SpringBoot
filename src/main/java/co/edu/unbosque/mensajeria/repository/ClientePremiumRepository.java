b12bd9aa48eab589396a9c6023c912809dbf4e6c
LisethBuitrago <lisethramireeez@gmail.com>
2026-03-21 09:18:08 -0500

    Repository Cliente normal y premium

--- src/main/java/co/edu/unbosque/mensajeria/repository/ClientePremiumRepository.java
+++ src/main/java/co/edu/unbosque/mensajeria/repository/ClientePremiumRepository.java
@@ -1,9 +1,24 @@
 package co.edu.unbosque.mensajeria.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import co.edu.unbosque.mensajeria.entity.ClientePremium;
 
 public interface ClientePremiumRepository extends CrudRepository<ClientePremium, Long> {
 
+
+	public Optional<List<ClientePremium>> findByNombre(String nombre);
+	public Optional<List<ClientePremium>> findByCedula(String cedula);
+	public Optional<List<ClientePremium>> findByCorreo(String correo);	
+	public Optional<List<ClientePremium>> findByTelefono(String telefono);		
+	public Optional<List<ClientePremium>> findByMetodoPago(String metodoPago);	
+	public Optional<List<ClientePremium>> findByTipoPedido(String tipoPedido);	
+	public Optional<List<ClientePremium>> findByTarifaPremium(double tarifaPremium);
+	
+	public Optional<List<ClientePremium>> findByNombreYCedula(String nombre, String cedula);	
+	public Optional<List<ClientePremium>> findByTipoPedidoYMetodoPago(String tipoPedido, String metodoPago);
+	
 }
