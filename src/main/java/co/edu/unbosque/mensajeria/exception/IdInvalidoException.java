8f91e2b5dbcd50dbaa2203cce4f63687dffb9223
avillarreala <tatikoavila23@gmail.com>
2026-03-22 15:14:37 -0500

    Excepcion id

---
+++ src/main/java/co/edu/unbosque/mensajeria/exception/IdInvalidoException.java
@@ -0,0 +1,8 @@
+package co.edu.unbosque.mensajeria.exception;
+
+public class IdInvalidoException extends RuntimeException {
+
+    public IdInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+}
