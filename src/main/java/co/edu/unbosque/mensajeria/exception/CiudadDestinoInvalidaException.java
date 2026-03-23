f87ef0b7522b7d4e762a8b0eed698f58567b5a12
avillarreala <tatikoavila23@gmail.com>
2026-03-23 09:16:42 -0500

    estado y ciudad

---
+++ src/main/java/co/edu/unbosque/mensajeria/exception/CiudadDestinoInvalidaException.java
@@ -0,0 +1,8 @@
+package co.edu.unbosque.mensajeria.exception;
+
+public class CiudadDestinoInvalidaException extends RuntimeException {
+
+    public CiudadDestinoInvalidaException(String mensaje) {
+        super(mensaje);
+    }
+}
