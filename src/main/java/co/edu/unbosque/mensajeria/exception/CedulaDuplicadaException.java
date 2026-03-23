650302528072208aeaf8fd8c48fb41f6ca291a47
avillarreala <tatikoavila23@gmail.com>
2026-03-23 10:20:37 -0500

    clase excepcion eliminada

--- src/main/java/co/edu/unbosque/mensajeria/exception/CedulaDuplicadaException.java
+++
@@ -1,7 +0,0 @@
-package co.edu.unbosque.mensajeria.exception;
-
-public class CedulaDuplicadaException extends RuntimeException {
-    public CedulaDuplicadaException(String message) {
-        super(message);
-    }
-}
