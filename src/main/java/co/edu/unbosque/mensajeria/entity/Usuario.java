a5281139c00be7b5a5427aca1521cb5d21438334
LisethBuitrago <lisethramireeez@gmail.com>
2026-03-23 17:15:30 -0500

    CREACION DE GET Y SET EN USUARIOOO
    INDISPENSABLE JAJAJA

--- src/main/java/co/edu/unbosque/mensajeria/entity/Usuario.java
+++ src/main/java/co/edu/unbosque/mensajeria/entity/Usuario.java
@@ -65,6 +65,16 @@ public abstract class Usuario {
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
+	
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 	@Override
 	public String toString() {
