ea6b638ff970363c9a2b07adc96897975f187b5e
sanchezgina0312 <sanchez.gina0312@gmail.com>
2026-03-23 19:53:17 -0500

    Correccion SQL
    Correccion SQL

--- src/main/java/co/edu/unbosque/mensajeria/entity/Paquete.java
+++ src/main/java/co/edu/unbosque/mensajeria/entity/Paquete.java
@@ -24,6 +24,7 @@ public abstract class Paquete {
 	private LocalDateTime fechaEstimadaEntrega;
 	private String ciudadDestino;
 	private String estadoPedido;
+	@jakarta.persistence.Column(name = "prioridad")
 	private boolean esPrioritario;
 	private double precioFinal;
 
