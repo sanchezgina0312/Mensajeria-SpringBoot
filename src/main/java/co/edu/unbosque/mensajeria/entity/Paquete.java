7854c636f07f311e6da7d34b717884f65e33f13b
sanchezgina0312 <sanchez.gina0312@gmail.com>
2026-03-23 20:07:07 -0500

    .
    .

--- src/main/java/co/edu/unbosque/mensajeria/entity/Paquete.java
+++ src/main/java/co/edu/unbosque/mensajeria/entity/Paquete.java
@@ -24,7 +24,7 @@ public abstract class Paquete {
 	private LocalDateTime fechaEstimadaEntrega;
 	private String ciudadDestino;
 	private String estadoPedido;
-	@jakarta.persistence.Column(name = "prioridad")
+	@jakarta.persistence.Column(name = "esPrioridad")
 	private boolean esPrioritario;
 	private double precioFinal;
 
