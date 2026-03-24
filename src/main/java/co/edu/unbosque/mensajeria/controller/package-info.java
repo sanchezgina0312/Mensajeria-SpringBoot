45d0d7282ca264b44817ea7a23029405890ae8d7
sanchezgina0312 <sanchez.gina0312@gmail.com>
2026-03-23 23:55:03 -0500

    JavaDoc clases faltantes
    JavaDoc clases faltantes

--- src/main/java/co/edu/unbosque/mensajeria/controller/package-info.java
+++ src/main/java/co/edu/unbosque/mensajeria/controller/package-info.java
@@ -1,2 +1,15 @@
-
+/**
+ * Contiene los **Controladores REST** de la aplicación.
+ * <p>
+ * Este paquete actúa como el punto de entrada para las peticiones externas. Se
+ * encarga de exponer los endpoints de la API, recibir las solicitudes HTTP,
+ * mapear los datos de entrada y retornar las respuestas adecuadas al cliente
+ * utilizando los servicios correspondientes.
+ *
+ * @author Gina B
+ * @author Gina S
+ * @author Angie V
+ * @author Natalia D
+ * @author Jairo E
+ */
 package co.edu.unbosque.mensajeria.controller;
--- src/main/java/co/edu/unbosque/mensajeria/service/package-info.java
+++ src/main/java/co/edu/unbosque/mensajeria/service/package-info.java
@@ -1,2 +1,15 @@
-
+/**
+ * Contiene las clases de la **Capa de Servicio** de la aplicación.
+ * <p>
+ * En este paquete se implementa la lógica de negocio y se coordinan las
+ * operaciones entre los controladores y los repositorios. Aquí se gestionan las
+ * reglas del sistema para el procesamiento de paquetes, asignación de
+ * trabajadores y validaciones complejas antes de la persistencia de datos.
+ *
+ * @author Gina B
+ * @author Gina S
+ * @author Angie V
+ * @author Natalia D
+ * @author Jairo E
+ */
 package co.edu.unbosque.mensajeria.service;
