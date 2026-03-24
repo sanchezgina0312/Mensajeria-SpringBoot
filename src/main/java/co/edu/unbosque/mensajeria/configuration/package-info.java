5bc2695625c19bdd4b4f12aa3295eeaceeb8502e
LisethBuitrago <lisethramireeez@gmail.com>
2026-03-23 23:05:31 -0500

    javadoc de paquete configuracion

--- src/main/java/co/edu/unbosque/mensajeria/configuration/package-info.java
+++ src/main/java/co/edu/unbosque/mensajeria/configuration/package-info.java
@@ -1,2 +1,11 @@
-
+/**
+ * Contiene las clases de configuración de la aplicación.
+ *
+ * @author Gina B
+ * @author Gina S
+ * @author Angie V
+ * @author Natalia D
+ * @author Jairo E
+ * @version 1.0
+ */
 package co.edu.unbosque.mensajeria.configuration;
