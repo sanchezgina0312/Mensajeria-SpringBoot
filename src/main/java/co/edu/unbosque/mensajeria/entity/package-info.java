38adf6038d01e4b6f0943aefa3eabfd1d9777a2c
LisethBuitrago <lisethramireeez@gmail.com>
2026-03-23 22:41:37 -0500

    Acabe javadoc entidades

--- src/main/java/co/edu/unbosque/mensajeria/entity/package-info.java
+++ src/main/java/co/edu/unbosque/mensajeria/entity/package-info.java
@@ -1,2 +1,17 @@
-
+/**
+ * Contiene las entidades de persistencia de la aplicación.
+ * <p>
+ * En este paquete se definen las clases que representan la estructura de la 
+ * base de datos relacional. Utiliza anotaciones de JPA (Jakarta Persistence API) 
+ * para el mapeo, permitiendo que la lógica del negocio 
+ * interactúe con los datos de manera transparente. Incluye la definición de 
+ * jerarquías de herencia para usuarios, trabajadores, clientes y paquetes.
+ *
+ * @author Gina B
+ * @author Gina S
+ * @author Angie V
+ * @author Natalia D
+ * @author Jairo E
+ * @version 1.0
+ */
 package co.edu.unbosque.mensajeria.entity;
