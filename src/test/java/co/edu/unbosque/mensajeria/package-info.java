6ca1a281aaeb90ea2a5e02d419c6e499fb680c7e
LisethBuitrago <lisethramireeez@gmail.com>
2026-03-24 17:09:49 -0500

    Create package-info.java

---
+++ src/test/java/co/edu/unbosque/mensajeria/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Contiene las clases encargadas de las **Pruebas Unitarias** de la aplicación.
+ * <p>
+ * En este paquete se implementan los casos de prueba para validar el comportamiento 
+ * aislado de los componentes del sistema. Su objetivo es asegurar que la lógica 
+ * de negocio, validaciones y transformaciones de datos funcionen correctamente, 
+ * utilizando dobles de prueba (mocks) para simular las dependencias externas.
+ *
+ * @author Gina B
+ * @author Gina S
+ * @author Angie V
+ * @author Natalia D
+ * @author Jairo E
+ */
+package co.edu.unbosque.mensajeria;
